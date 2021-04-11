c9a957aac4cc2c1c7b6e25f34f3a718e4c0c8fc1
Aitor Vidal Arnau <showzen@hotmail.es>
2021-04-11 17:43:49 +0200

    Creando proyecto de prueba en STS para github

---
+++ PruebagithubSTS/src/main/java/com/aitor/pruebagithubsts/PruebagithubStsApplication.java
@@ -0,0 +1,13 @@
+package com.aitor.pruebagithubsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebagithubStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebagithubStsApplication.class, args);
+	}
+
+}
---
+++ PruebagithubSTS/src/test/java/com/aitor/pruebagithubsts/PruebagithubStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitor.pruebagithubsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebagithubStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
